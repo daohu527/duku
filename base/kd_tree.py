56f063ea618d06450ff8936498d2fc9322f64483
daohu527 <daohu527@gmail.com>
2021-12-08 17:05:32 +0800

    base: add query_nearest for kdtree

--- base/kd_tree.py
+++ base/kd_tree.py
@@ -23,20 +23,61 @@ def get_k_median(arr, axis):
   arr.sort(key=itemgetter(axis))
   return len(arr) // 2
 
+def euclidean_distance(arr1, arr2):
+  assert len(arr1) == len(arr2), "Elements are not equal in size!"
+  sum_square = 0
+  for x1, x2 in zip(arr1, arr2):
+    sum_square += pow(x1 - x2, 2)
+  return pow(sum_square, 0.5)
+
 class Node:
-  def __init__(self, val) -> None:
-    self.val = val
+  def __init__(self, element, axis) -> None:
+    self.element = element
+    self.axis = axis
+    self.value = element[axis]
     self.left = None
     self.right = None
 
+def _query_nearest(point, node, nearest, distance):
+  if node is None:
+    return
+
+  if node.left is None and node.right is None:
+    t_distance = euclidean_distance(node.element, point)
+    if t_distance < distance[0]:
+      distance[0] = t_distance
+      nearest[0] = node.element
+    return
+
+  if point[node.axis] <= node.value:
+    if point[node.axis] - distance[0] <= node.value:
+      _query_nearest(point, node.left, nearest, distance)
+    if point[node.axis] + distance[0] > node.value:
+      _query_nearest(point, node.right, nearest, distance)
+  else:
+    if point[node.axis] + distance[0] > node.value:
+      _query_nearest(point, node.right, nearest, distance)
+    if point[node.axis] - distance[0] <= node.value:
+      _query_nearest(point, node.left, nearest, distance)
+
 class KdTree:
   def __init__(self, points = None) -> None:
-    if not points:
+    if points:
       assert len(points[0]) != 0, "Element is empty!"
       self._k = len(points[0])
       self._root = self.construct(points, 0)
     else:
       self._root = None
+
+  def construct(self, points, depth) -> Node:
+    if not points:
+        return None
+    axis = depth % self._k
+    median = get_k_median(points, axis)
+    node = Node(points[median], axis)
+    node.left = self.construct(points[:median], depth + 1)
+    node.right = self.construct(points[median+1:], depth + 1)
+    return node
 
   def add(self, point) -> bool:
     assert self._root, "KdTree is empty!"
@@ -50,6 +91,15 @@ class KdTree:
     assert self._root, "KdTree is empty!"
     pass
 
+  def query_nearest(self, point) -> Node:
+    assert self._root, "KdTree is empty!"
+
+    # Need to pass by ref, so we pass a list
+    nearest = [None]
+    distance = [2**31 - 1] # init max value
+    _query_nearest(point, self._root, nearest, distance)
+    return nearest[0], distance[0]
+
   def query_radius(self, radius) -> List[Node]:
     assert self._root, "KdTree is empty!"
     pass
@@ -60,25 +110,21 @@ class KdTree:
     while cur_s:
       next_s = []
       for c in cur_s:
-        print(c.val)
+        print(c.element, c.axis)
         if c.left:
           next_s.append(c.left)
         if c.right:
           next_s.append(c.right)
       cur_s = next_s
 
-  def construct(self, points, depth) -> Node:
-    if not points:
-        return None
-    axis = depth % self._k
-    median = get_k_median(points, axis)
-    node = Node(points[median])
-    node.left = self.construct(points[:median], depth + 1)
-    node.right = self.construct(points[median+1:], depth + 1)
-    return node
-
 
 if __name__ == '__main__':
   points = [(7, 2), (5, 4), (9, 6), (4, 7), (8, 1), (2, 3)]
   tree = KdTree(points)
   print(tree.display())
+
+  print(euclidean_distance((1,2),(3,4)))
+
+  nearest, distance = tree.query_nearest((1,2))
+  print(nearest)
+  print(distance)
